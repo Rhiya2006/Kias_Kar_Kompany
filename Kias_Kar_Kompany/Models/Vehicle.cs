4dd384629b6dbe8c13deab765ab05940f8c17eea
Rhiya2006 <165028183+Rhiya2006@users.noreply.github.com>
2026-04-14 21:16:48 +0200

    Commit - Ice Task 2

--- Kias_Kar_Kompany/Models/Vehicle.cs
+++ Kias_Kar_Kompany/Models/Vehicle.cs
@@ -6,12 +6,21 @@ namespace Kias_Kar_Kompany.Models
     {
         [Key]
         public int VehicleId { get; set; }
+
         public required string VehicleName { get; set; }
         public required string VehicleModel { get; set; }
         public required int VehiclePrice { get; set; }
         public required string VehicleType { get; set; } = "Sedan";
         public string? VehicleImageURL { get; set; }
 
+        [Required]
+        public int? ManufacturerId { get; set; }
+
+        [Required]
+        public int? OwnerId { get; set; }
+
+        //navigation - vehicle belongs to one manufacturer
         public Manufacturer? Manufacturer { get; set; }
+        public Owner? Owner { get; set; }
     }
 }
